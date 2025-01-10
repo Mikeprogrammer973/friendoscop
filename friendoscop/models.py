0b87e5ab936427abe03d60a7d54ce15b8d89f799
Mike <antiquesclub007@gmail.com>
2025-01-10 15:41:45 -0500

    db models

--- friendoscop/models.py
+++ friendoscop/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    registration_field = models.CharField(max_length=10)
+    last_name = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=30)
+    birth_date = models.DateField()
+    email = models.EmailField()
+    home_phone_number = models.CharField(max_length=10)
+    cell_phone_number = models.CharField(max_length=10)
+    password = models.CharField(max_length=64)
+    friends = models.ManyToManyField('self')
+    faculty = models.ForeignKey('Faculty', on_delete=models.CASCADE, default=None, null=True)
+
+class Message(models.Model):
+    author = models.ForeignKey('Person', on_delete=models.CASCADE)
+    content = models.TextField()
+    publication_date = models.DateField()
+
+class Faculty(models.Model):
+    name = models.CharField(max_length=30)
+    color = models.CharField(max_length=10)
+
+class Campus(models.Model):
+    name = models.CharField(max_length=30)
+    address = models.CharField(max_length=70)
+
+class Cursus(models.Model):
+    title = models.CharField(max_length=30)
+
+class Job(models.Model):
+    title = models.CharField(max_length=30)
+
+class Employee(Person):
+    office = models.CharField(max_length=30)
+    campus = models.ForeignKey('Campus', on_delete=models.CASCADE)
+    job = models.ForeignKey('Job', on_delete=models.CASCADE)
+
+class Student(Person):
+    cursus = models.ForeignKey('Cursus', on_delete=models.CASCADE)
+    year = models.IntegerField()
